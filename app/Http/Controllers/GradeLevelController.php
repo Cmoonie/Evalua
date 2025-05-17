a1b1b2c2203e56811fcef44efd2c1b3ea698f89c
ninameier1 <s1081087@student.windesheim.nl>
2025-05-17 11:54:56 +0200

    Create models, controllers, factories

---
+++ app/Http/Controllers/GradeLevelController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\grade_level;
+use Illuminate\Http\Request;
+
+class GradeLevelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(grade_level $grade_level)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(grade_level $grade_level)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, grade_level $grade_level)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(grade_level $grade_level)
+    {
+        //
+    }
+}
---
+++ app/Models/filled_component.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class filled_component extends Model
+{
+    //
+}
