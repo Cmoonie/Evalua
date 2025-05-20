98987caae3cc9d2da34b20459b6840a96824590e
ninameier1 <s1081087@student.windesheim.nl>
2025-05-20 12:07:29 +0200

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComponentController;
+use App\Http\Controllers\CompetencyController;
+use App\Http\Controllers\FormController;
+use App\Http\Controllers\GradeLevelController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +20,12 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// CRUD routes voor onze controllers, misschien niet nodig?
+Route::resource('competencies', CompetencyController::class);
+Route::resource('components', ComponentController::class);
+Route::resource('forms', FormController::class);
+Route::resource('gradelevels', GradeLevelController::class);
+
 
 require __DIR__.'/auth.php';
