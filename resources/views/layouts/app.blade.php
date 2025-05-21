1597a1b86e6aa416d95082f657502d03ce17faed
ninameier1 <s1081087@student.windesheim.nl>
2025-05-21 14:38:00 +0200

    Update front end and add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <!-- Titel -->
+        <title>{{ config('app.name', 'Evalua') }}</title>
+
+        <!-- Favicon-->
+        <link rel="icon" href="{{ asset('favicon.ico') }} type="image/x-icon"">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
