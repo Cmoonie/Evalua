d7b78d88f332f3c154af4b903803b8fc2e0e9fb7
ninameier1 <s1081087@student.windesheim.nl>
2025-05-24 12:15:57 +0200

    Backup voor merge

---
+++ app/Http/Requests/StoreFilledFormRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreFilledFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            // De data die uit het formulier komt
+            'form_id'      => ['required', Rule::exists('forms', 'id')],
+            'student_name' => ['required', 'string', 'max:64'],
+
+            // Componenten
+            'components'                   => ['required', 'array'],
+            'components.*.component_id'    => ['required', Rule::exists('components', 'id')],
+            'components.*.grade_level_id'  => ['required', Rule::exists('grade_levels', 'id')],
+            'components.*.comment'         => ['nullable', 'string'],
+        ];
+    }
+}
